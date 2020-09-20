a67f75d4d22dbc8fdb81c30a872ea03efbbf4eb8
Syn <45761547+SynCode05@users.noreply.github.com>
2020-09-20 14:40:58 +0500

    Fixed a bug.

--- modules/onEvent.js
+++ modules/onEvent.js
@@ -1,17 +1,16 @@
 const dashboard = require('../dashboard/app');
 
 module.exports.run = (client) => {
-    client.on('ready', async () => {
-        
-        dashboard(client);
 
+    dashboard(client);
+
+    client.on('ready', async () => {
         client.editStatus('online', {
             name: `syn coding me.`, 
             type: 3,
         });
 
         client.logger.ready(`Logged in as ${client.user.username} | Users: ${client.users.size} | Guilds: ${client.guilds.size}`);
-
     });
 
     client.on('disconnect', () => client.logger.disconnect(`Disconnected from ${client.user.username}`));
